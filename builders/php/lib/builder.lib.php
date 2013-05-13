5583e7a012fd715db42fc924bc2416c4186c8c75
Dave Olsen <dmolsen@gmail.com>
2013-05-13 11:00:37 -0400

    re-organizing the directory structure stuff

--- builders/php/lib/builder.lib.php
+++ builders/php/lib/builder.lib.php
@@ -41,11 +41,13 @@ class Builder {
 		require __DIR__."/mustache/Autoloader.php";
 		Mustache_Autoloader::register();
 		
-		$this->m = new Mustache_Engine(array(
-			'loader' => new Mustache_Loader_PatternLoader(__DIR__."/"),
-			"partials_loader" => new Mustache_Loader_PatternLoader(__DIR__."/../../../source/patterns")
+	}
+	
+	protected function mustacheInstance() {
+		return new Mustache_Engine(array(
+			'loader' => new Mustache_Loader_PatternLoader(__DIR__.$this->sp),
+			"partials_loader" => new Mustache_Loader_PatternLoader(__DIR__.$this->sp)
 		));
-		
 	}
 	
 	/**
@@ -59,12 +61,12 @@ class Builder {
 		}
 		
 		// gather data from pattern/data.json
-		$entries = scandir(__DIR__."/".$this->sp);
+		$entries = scandir(__DIR__.$this->sp);
 		foreach($entries as $entry) {
 			if (!in_array($entry,$this->if)) {
-				if (file_exists(__DIR__."/".$this->sp.$entry."/data.json")) {
+				if (file_exists(__DIR__.$this->sp.$entry."/data.json")) {
 					$d = new stdClass();
-					$d->$entry = json_decode(file_get_contents(__DIR__."/".$this->sp.$entry."/data.json"));
+					$d->$entry = json_decode(file_get_contents(__DIR__.$this->sp.$entry."/data.json"));
 					$this->d = (object) array_merge((array) $this->d, (array) $d);
 				}
 			}
@@ -72,30 +74,33 @@ class Builder {
 	
 	}
 	
-	protected function renderPattern($f) {
-		return $this->m->render($f,$this->d);
+	protected function renderPattern($f,$m) {
+		return $m->render($f,$this->d);
 	}
 	
-	private function renderFile($f) {
-		$h  = file_get_contents(__DIR__."/".$this->sp."d-wrapper/header.html");
-		$rf = $this->renderPattern($f);
-		$f  = file_get_contents(__DIR__."/".$this->sp."d-wrapper/footer.html");
+	private function renderFile($f,$m) {
+		$h  = file_get_contents(__DIR__.$this->sp."d-wrapper/header.html");
+		$rf = $this->renderPattern($f,$m);
+		$f  = file_get_contents(__DIR__.$this->sp."d-wrapper/footer.html");
 		return $h."\n".$rf."\n".$f;
 	}
 	
 	protected function renderAndMove() {
-		$entries = scandir(__DIR__."/".$this->sp);
+		
+		$m = $this->mustacheInstance();
+		
+		$entries = scandir(__DIR__.$this->sp);
 		foreach($entries as $entry) {
 			if (!in_array($entry,$this->if)) {
-				if (file_exists(__DIR__."/".$this->sp.$entry."/pattern.mustache")) {
-					$r = $this->renderFile($this->sp.$entry."/pattern.mustache");
-					if (!is_dir(__DIR__."/".$this->pp.$entry)) {
-						mkdir(__DIR__."/".$this->pp.$entry);
+				if (file_exists(__DIR__.$this->sp.$entry."/pattern.mustache")) {
+					$r = $this->renderFile($entry."/pattern.mustache",$m);
+					if (!is_dir(__DIR__.$this->pp.$entry)) {
+						mkdir(__DIR__.$this->pp.$entry);
 						//chmod($this->pp.$entry,$this->dp);
-						file_put_contents(__DIR__."/".$this->pp.$entry."/pattern.html",$r);
+						file_put_contents(__DIR__.$this->pp.$entry."/pattern.html",$r);
 						//chmod($this->pp.$entry."/pattern.html",$this->fp);
 					} else {
-						file_put_contents(__DIR__."/".$this->pp.$entry."/pattern.html",$r);
+						file_put_contents(__DIR__.$this->pp.$entry."/pattern.html",$r);
 					}
 				}
 			}
